917cebf26d647b455c04ea833216f9dadccb640f
Bess L. Walker <blwalker@egr.msu.edu>
2013-02-22 14:28:26 -0500

    Adds unsophisticated assertion-based testing.

---
+++ tests.py
@@ -0,0 +1,23 @@
+# Very basic assert-based testing for the flight routing problem.
+# Bess L. Walker
+# 2-22-13
+
+import routing
+
+# Initialize some basic points & edges
+p1 = routing.City('a', 0, 0)
+p2 = routing.City('b', 1, 1)
+
+e12 = routing.Leg(p1, p2)
+e21 = routing.Leg(p2, p1)
+
+# Check __repr__ methods
+assert repr(p1) == "<City:a(0,0)>"
+assert repr(e21) == "<Leg:b->a>"
+
+# Check distance calculations
+# I'm not sure these are actually reliable, what with floating point math and all.
+assert p1.distance_to(p1) == 0.0
+assert p1.distance_to(p2) == p2.distance_to(p1)
+
+assert e21.miles == p2.distance_to(p1)
